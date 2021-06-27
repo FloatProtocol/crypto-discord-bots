65d4c42a7040f123c8e646a1c96d15c75ba54748
FloatCoder <77080072+floatcoder@users.noreply.github.com>
2021-06-27 22:28:28 +0100

    feat: update gas bot to use WS endpoint

---
+++ src/gasBot.ts
@@ -0,0 +1,55 @@
+import { Client } from "discord.js"
+import WebSocket from "ws"
+
+const GAS_NOW_WS ='wss://www.gasnow.org/ws';
+
+interface GasData {
+  gasPrices: {
+    rapid: number;
+    fast: number;
+    standard: number;
+    slow: number;
+  }
+}
+
+function fromGwei(gwei: number): string {
+  return (gwei / 1e9).toFixed(2);
+}
+
+function updateGasPrice(bot: Client, gasData: GasData) {
+  const prices = gasData.gasPrices;
+  bot.guilds.cache.forEach(async (guild) => {
+    console.log(`Setting gas ${fromGwei(prices.standard)} in ${guild.me}`);
+    const botMember = guild.me
+    await botMember?.setNickname(`⛽ ${fromGwei(prices.standard)} gwei`)
+  })
+
+  bot.user?.setActivity(
+    `🐢 ${fromGwei(prices.slow)} || 🐇 ${fromGwei(prices.rapid)}`,
+    { type: "PLAYING" },
+  )
+}
+
+export const newGasBot = (): Client => {
+  const gasBot = new Client()
+  const gasWs = new WebSocket(GAS_NOW_WS)
+
+  // Init logs
+  gasBot.on("ready", () => {
+    console.log(`Bot successfully started as ${gasBot.user?.tag} 🤖`)
+  })
+  gasWs.onopen = (evt) => {
+    console.log(`Connection open to ${GAS_NOW_WS}...`);
+  };
+
+  gasWs.onmessage = (evt) => {
+    const dataStr = evt.data;
+    const data = JSON.parse(dataStr.toString());
+
+    if (data.type) {
+      updateGasPrice(gasBot, data.data);
+    }
+  }
+
+  return gasBot
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,9 @@
-import { Client } from "discord.js"
+
 import dotenv from "dotenv"
 
-import { fetchGas } from "./services"
+import { newGasBot } from "./gasBot";
 
 dotenv.config()
 
-const GAS_POLLING_INTERVAL = 60 * 1000 // 60 seconds
-
-const gasBot = new Client()
-
-gasBot.on("ready", () => {
-  console.log(`Bot successfully started as ${gasBot.user?.tag} 🤖`)
-})
-
-// Updates token price on bot's nickname every X amount of time
-gasBot.setInterval(async () => {
-  const data = await fetchGas()
-
-  if (!data) return
-
-  const { gasPriceGwei, gasPriceUsd } = data
-
-  console.log(`Updating gas`);
-
-  gasBot.guilds.cache.forEach(async (guild) => {
-    console.log(`Setting nickname in ${guild.me}`);
-    const botMember = guild.me
-    await botMember?.setNickname(`Gas: ${gasPriceGwei} gwei`)
-  })
-
-  gasBot.user?.setActivity(
-    `transfer fee: $${gasPriceUsd.toFixed(2)}`,
-    { type: "WATCHING" },
-  )
-}, GAS_POLLING_INTERVAL)
-
-gasBot.login(process.env.DISCORD_API_TOKEN)
+const gasBot = newGasBot()
+gasBot.login(process.env.GAS_BOT_API_KEY)
