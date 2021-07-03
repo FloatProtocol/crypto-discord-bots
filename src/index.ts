992be99ac5497f559e74e411364664a2c5ebf965
FloatCoder <77080072+floatcoder@users.noreply.github.com>
2021-07-03 17:56:19 +0100

    feat: only enable bots if their respective env var is found

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,72 @@
 
+import { Client } from "discord.js"
 import dotenv from "dotenv"
-import { newBlockNumberBot } from "./blockNumberBot"
 
+import { newBlockNumberBot } from "./blockNumberBot"
 import { newGasBot } from "./gasBot"
 import { newMarketCapBot } from "./marketCapBot"
 import { newPriceBot } from "./priceBot"
+import { newAuctioneerBot } from "./auctioneerBot"
 
 dotenv.config()
 
 const BANK_ID = "float-protocol"
 const FLOAT_ID = "float-protocol-float"
 
-const gasBot = newGasBot()
-const blockBot = newBlockNumberBot()
-const bankMarketCapBot = newMarketCapBot(BANK_ID)
-const bankPriceBot = newPriceBot(BANK_ID)
-const floatMarketCapBot = newMarketCapBot(FLOAT_ID)
-const floatPriceBot = newPriceBot(FLOAT_ID)
+interface BotFactory {
+  init(...args: unknown[]): Client | undefined;
+  args?: unknown[];
+  key?: string;
+}
 
-// const auctionBot = newAuctioneerBot()
+const BOT_FACTORIES: BotFactory[] = [
+  {
+    init: newGasBot,
+    key: process.env.GAS_BOT_API_KEY,
+  },
+  {
+    init: newBlockNumberBot,
+    key: process.env.BLOCK_BOT_API_KEY,
+  },
+  {
+    init: newMarketCapBot,
+    args: [BANK_ID],
+    key: process.env.BANK_MC_BOT_API_KEY
+  },
+  {
+    init: newMarketCapBot,
+    args: [FLOAT_ID],
+    key: process.env.FLOAT_MC_BOT_API_KEY
+  },
+  {
+    init: newPriceBot,
+    args: [BANK_ID],
+    key: process.env.BANK_PRICE_BOT_API_KEY
+  },
+  {
+    init: newPriceBot,
+    args: [FLOAT_ID],
+    key: process.env.FLOAT_PRICE_BOT_API_KEY
+  },
+  {
+    init: newAuctioneerBot,
+    key: process.env.AUCTIONEER_BOT_API_KEY
+  }
+]
 
-// Logins
-gasBot.login(process.env.GAS_BOT_API_KEY)
-blockBot.login(process.env.BLOCK_BOT_API_KEY)
+function buildBot(factory: BotFactory) {
+  if (!factory.key) {
+    return
+  } 
 
-bankMarketCapBot.login(process.env.BANK_MC_BOT_API_KEY)
-floatMarketCapBot.login(process.env.FLOAT_MC_BOT_API_KEY)
+  const bot = factory.init(factory.args)
 
-bankPriceBot.login(process.env.BANK_PRICE_BOT_API_KEY)
-floatPriceBot.login(process.env.FLOAT_PRICE_BOT_API_KEY)
+  if (!bot) {
+    return
+  }
 
-// auctionBot.login(process.env.AUCTIONEER_BOT_API_KEY)
+  bot.login(factory.key)
+  return bot
+}
+
+BOT_FACTORIES.forEach(buildBot)
